1b90622bf1071f32021cc2860c0f2945eb708ac9
Manny B <thesmasher790@gmail.com>
2021-12-21 18:50:57 +0200

    updated firebase config

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_apiKey,
-	authDomain: process.env.NEXT_PUBLIC_authDomain,
-	projectId: process.env.NEXT_PUBLIC_projectId,
-	storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-	messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-	appId: process.env.NEXT_PUBLIC_appId,
-	measurementId: process.env.NEXT_PUBLIC_measurementId,
-}
+	apiKey: "AIzaSyC5ig0hdxvlACwIwfTmi1DEwOzQHO27h_0",
+	authDomain: "cinema-hub.firebaseapp.com",
+	projectId: "cinema-hub",
+	storageBucket: "cinema-hub.appspot.com",
+	messagingSenderId: "552348185836",
+	appId: "1:552348185836:web:fa6167e2ebe6581731a1c9",
+	measurementId: "G-EEC6433XJ9"
+  }
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
