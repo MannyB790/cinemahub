cbab41147b4313f389a2f919769b7ae65e30fba7
Manny B <thesmasher790@gmail.com>
2021-12-21 18:55:26 +0200

    fixed import

--- pages/shows/index.tsx
+++ pages/shows/index.tsx
@@ -1,7 +1,6 @@
 import { Fragment } from 'react'
 import MPTShows from '../../src/components/layout/Movies/Movies'
 import SearchBar from '../../src/components/layout/navigation/SearchBar'
-import LUShows from '../../src/components/layout/Shows/LUShows'
 
 const Shows = () => {
 	return (
